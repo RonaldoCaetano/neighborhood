311a1ee967f65223cfe54abdeb799c73607d9233
RonaldoCaetano <ronaldogodoi2@gmail.com>
2021-02-24 23:23:47 -0300

    adding repositories

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,45 @@
-import React, { FC } from "react";
+import { FC, useEffect, useState } from "react";
+
+import "./styles.css";
 
 const App: FC = () => {
+  const [userName, setUserName] = useState("");
+  const [userRepositories, setUserRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    if (userName !== "") {
+      fetch(`https://api.github.com/users/${userName}/repos`)
+        .then((data) => data.json())
+        .then((repositories) => setUserRepositories(repositories));
+    } else {
+      setUserRepositories([]);
+    }
+  }, [userName]);
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container">
+      <label htmlFor="github-username">Github Username</label>
+      <input
+        type="text"
+        id="github-username"
+        name="github-username"
+        onBlur={(e) => setUserName(e.currentTarget.value)}
+      />
+
+      <section className="repositoriesContainer">
+        {userRepositories.map((repository) => (
+          <article key={repository.id} className="repository">
+            <p className="repositoryContentText">
+              <strong>Repository Name: </strong>
+              <span>{repository.name}</span>
+            </p>
+            <p className="repositoryContentText">
+              <strong>Repository URL: </strong>
+              <span>{repository.url}</span>
+            </p>
+          </article>
+        ))}
+      </section>
     </div>
   );
 };
---
+++ src/typings/repositories.d.ts
@@ -0,0 +1,29 @@
+interface Repository {
+  clone_url: string;
+  created_at: string;
+  description: string | null;
+  disabled: boolean;
+  downloads_url: string;
+  events_url: string;
+  full_name: string;
+  git_url: string;
+  homepage: string | null;
+  id: number;
+  language: string;
+  languages_url: string;
+  name: string;
+  owner: RepositoryOwner;
+  private: false;
+  pushed_at: string;
+  size: number;
+  ssh_url: string;
+  updated_at: string;
+  url: string;
+}
+
+interface RepositoryOwner {
+  login: string;
+  id: number;
+  avatar_url: string;
+  html_url: string;
+}
